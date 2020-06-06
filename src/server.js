a18320da7f786047c3124c2b7808a4ac4f711797
Lucio palmieri <65705360+luciopalm@users.noreply.github.com>
2020-06-06 19:38:48 -0300

    end day 4

--- src/server.js
+++ src/server.js
@@ -1,15 +1,35 @@
 const express = require("express")
 const server = express()
 
+//Configurar pasta publica
+
+server.use(express.static("public"))
+
+// utilizando template engine
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //Configurar caminhos  da minha aplicção
 //Pagina inicial
 //req:requisição
 //res:resposta
 
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
-
+    return res.render("index.html", { title: "Um Título"})
 })
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
+})
+
 
 
 
