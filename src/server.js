25fa456dd39ed68886282169a8edd6bb9a08e107
Lucio palmieri <65705360+luciopalm@users.noreply.github.com>
2020-06-06 16:27:38 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,19 @@
 const express = require("express")
 const server = express()
 
+//Configurar caminhos  da minha aplicção
+//Pagina inicial
+//req:requisição
+//res:resposta
+
+server.get("/", (req, res) => {
+    res.send("Hello world")
+
+})
+
+
+
+
 //Ligar o servidor
 
 server.listen(3000)
