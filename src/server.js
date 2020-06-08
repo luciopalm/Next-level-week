a02167967ca355d381223dc15e3bbd575030bf0f
Lucio palmieri <65705360+luciopalm@users.noreply.github.com>
2020-06-07 21:59:14 -0300

    END PROJECT

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ const db = require("./database/db")
 
 server.use(express.static("public"))
 
+//habilitar o uso do req.body
+
+server.use(express.urlencoded({ extended: true}))
+
 // utilizando template engine
 
 const nunjucks = require("nunjucks")
@@ -25,15 +29,75 @@ server.get("/", (req, res) => {
     return res.render("index.html", { title: "Um Título"})
 })
 
+
+
+
 server.get("/create-point", (req, res) => {
+   
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+      
+    //req.body: o corpo do nosso formulario
+    //console.log(req.body)
+    //inserir dados no banco de dados:
+
+    //inserir dados na tabela
+    const query = `
+        INSERT INTO places (
+         image, 
+         name, 
+         address,
+         address2,
+         state,
+         city,
+         items
+         ) VALUES (?,?,?,?,?,?,?);
+
+`       
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items, 
+
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso!")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+
+    }
+    db.run(query, values, afterInsertData) 
+    
+})
+
+
+
+
 server.get("/search", (req, res) => {
+
+    const search = req.query.search
+
+    if(search == "") {
+        //pesquisa vazia
+        return res.render("search-results.html", {total: 0})
+    }
 
     //Pegar os dados do banco de dados
 
-    db.all(`SELECT * FROM places`, function(err, rows) {
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
         if(err) {
             return console.log(err)
         }
